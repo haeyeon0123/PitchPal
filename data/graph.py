d80cdb2e7ec599454d59ad25f3ca44c3e35bf529
DESKTOP-5M8P3KI\lhy27 <lhy2709@gmail.com>
2025-05-30 14:27:23 +0900

    graph change

--- data/graph.py
+++ data/graph.py
@@ -1,6 +1,20 @@
 import pandas as pd
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('data file name', index_col=0)
-df.plot(kind='line')
-plt.show() 
+# 데이터 불러오기
+df = pd.read_csv('data/PitchPal_survey.csv')  # \ 대신 / 사용 (cross-platform 호환성 위해)
+df.index.name = 'Index'  # x축 이름 설정 (선택 사항)
+
+# 필요한 컬럼만 선택
+columns_to_plot = ['발화 속도', 'wps', 'char_speed']
+
+# 선 그래프 (Line Plot)
+plt.figure(figsize=(12, 6))
+for col in columns_to_plot:
+    plt.plot(df.index, df[col], label=col)
+plt.title('speech speed evaluation')
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.legend()
+plt.grid(True)
+plt.show()
