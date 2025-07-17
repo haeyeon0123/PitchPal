f55c12bda9318f8a3362c81c72df4d21e76f1f6c
DESKTOP-5M8P3KI\lhy27 <lhy2709@gmail.com>
2025-07-18 00:12:27 +0900

    sample 파일 재변경

--- model/speech/main.py
+++ model/speech/main.py
@@ -3,8 +3,8 @@ from core.speech_analysis import analyze_speech, detect_filler_words
 #from pause_ratio_calculator import calculate_pause_ratio
 
 if __name__ == "__main__":
-    audio_path = "data/pitch_sample2.m4a"
+    audio_path = "data/pitch_sample.m4a"
     script_path = "data/pitch_sample_script.txt"
-    detect_filler_words(audio_path)
-    #analyze_speech(audio_path, script_path)
+    #detect_filler_words(audio_path)
+    analyze_speech(audio_path, script_path)
     #calculate_pause_ratio(audio_path)
