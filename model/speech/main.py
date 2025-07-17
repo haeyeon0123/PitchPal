3332de04f21ddcd5063471c0932986fd3e394f77
DESKTOP-5M8P3KI\lhy27 <lhy2709@gmail.com>
2025-07-17 21:32:10 +0900

    오류 수정

--- model/speech/main.py
+++ model/speech/main.py
@@ -1,5 +1,5 @@
 # 실행
-from model.speech.core.speech_analysis import analyze_speech
+from core.speech_analysis import analyze_speech
 #from pause_ratio_calculator import calculate_pause_ratio
 
 if __name__ == "__main__":
