2196e9e8dabcb6d9b589b8ceb9c83eb8f19b3e52
pse9684 <pse9684@gmail.com>
2025-07-29 16:22:56 +0000

    발화 속도 및 발음 정확도 평가 예측 모델

--- model/speech/main.py
+++ model/speech/main.py
@@ -1,17 +1,31 @@
-# 실행
-import time
 from core.speech_analysis import analyze_speech
 from core.stt_pronunciation import load_whisper_model
+import joblib
+import time
 
 if __name__ == "__main__":
-    total_start = time.time()  # ⏱ 전체 시작 시간 기록
+    start = time.time()
 
-    audio_path = "data/test1.m4a"
-    script_path = "data/test1.txt"
+    audio_path = "data/pitch_sample.m4a"
+    script_path = "data/pitch_sample_script.txt"
+    model = load_whisper_model("small")
 
-    model = load_whisper_model("small") 
+    # 음성 피처 추출
+    features = analyze_speech(audio_path, script_path, model)
+    wpm = features["wpm"]
+    pause_ratio = features["pause_ratio"]
+    pron_score = features["pron_score"]
 
-    analyze_speech(audio_path, script_path, model)
+    # 모델 불러오기
+    model_speed = joblib.load("model/evaluation/model_speed.pkl")
+    model_pron = joblib.load("model/evaluation/model_pron.pkl")
 
-    total_end = time.time()  # ⏱ 전체 종료 시간 기록
-    print(f"\n⏱ 총 실행 시간        : {total_end - total_start:.2f}초")
+    # 예측
+    predicted_speed = model_speed.predict([[wpm, pause_ratio]])[0]
+    predicted_pron = model_pron.predict([[pron_score]])[0]
+
+    # 출력
+    print(f"\n✅ 예측된 발화 속도     : {predicted_speed:.2f}")
+    print(f"✅ 예측된 발음 정확도   : {predicted_pron:.2f}")
+
+    print(f"\n⏱ 총 실행 시간: {time.time() - start:.2f}초")
