9ccf58d5d7e2ce4b7182d9274c0a310226889ae2
DESKTOP-5M8P3KI\lhy27 <lhy2709@gmail.com>
2025-09-02 16:03:03 +0900

    requirements_new.txt 추가

--- model/speech/main_temp.py
+++ model/speech/main_temp.py
@@ -1,6 +1,6 @@
-from core.stt_pronunciation import transcribe_audio, load_whisper_model
-from core.filler_words import detect_filler_words
-from core.speech_analysis import analyze_speech, save_segment_features_to_json
+from model.speech.core.stt_pronunciation import transcribe_audio, load_whisper_model
+from model.speech.core.filler_words import detect_filler_words
+from model.speech.core.speech_analysis import analyze_speech, save_segment_features_to_json
 import time
 
 if __name__ == "__main__":
